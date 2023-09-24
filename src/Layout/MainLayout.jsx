fc249962d7d807b5a2eb4f49fd5f0bfe47d9b9b3
jasaiful <jasaiful@gmail.com>
2023-09-25 00:04:14 +0600

    created initial react file

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,11 @@
+
+
+const MainLayout = () => {
+    return (
+        <div>
+            <div className="text-3xl font-bold text-center underline">Hello world!</div>
+        </div>
+    );
+};
+
+export default MainLayout;
